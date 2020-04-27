f547ca12e9fbd6e7da24a29f3a7051fa2a7db553
ACH322 <achareun22@gmail.com>
2020-04-27 09:50:44 -0300

    add pokemon as a class

---
+++ src/pokemon-class.js
@@ -0,0 +1,14 @@
+class Pokemon {
+  constructor({ name, id, sprites, height, weight, types, abilities, stats }) {
+    this.name = name;
+    this.id = id;
+    this.sprites = sprites;
+    this.height = height;
+    this.weight = weight;
+    this.types = types;
+    this.abilities = abilities;
+    this.stats = stats;
+  }
+}
+
+export default Pokemon;
